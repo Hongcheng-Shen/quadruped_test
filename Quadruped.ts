d3fa8b8cd72027b3670f135db0dd694f78ee3dd2
Hongcheng-Shen <49156279+Hongcheng-Shen@users.noreply.github.com>
2021-06-28 18:05:14 +0800

    更新 pxt.json, main.blocks, main.ts, Quadruped.ts, _locales/zh-CN/Quadruped-strings.json

--- Quadruped.ts
+++ Quadruped.ts
@@ -3,18 +3,18 @@
 /**
  * robot blocks
  */
-//% weight= 0 color=#0abcff icon="\uf207" block="Robot"
-//% groups='["Positional","Advanced"]'
-namespace Robot {
+//% weight= 0 color=#0abcff icon="\uf207" block="Quadruped"
+//% groups='["control","Advanced"]'
+namespace Quadruped {
     /**
      * TODO: describe your function here
      * @param n describe parameter here, eg: 5
      * @param s describe parameter here, eg: "Hello"
      * @param e describe parameter here
      */
-    //% groups="Positional"
+    //% group="control"
     //% blockGap=8
-    //% blockId=Robot_init block="go straight at full speed"
+    //% blockId=Quadruped_init block="Quadruped init"
     export function init(): void {
         let i = 0
         // Add code here
@@ -25,9 +25,9 @@ namespace Robot {
     * @param s describe parameter here, eg: "Hello"
     * @param e describe parameter here
     */
-    //% groups="Advanced"
+    //% group="Advanced"
     //% blockGap=8
-    //% blockId=Robot_aa block="aa"
+    //% blockId=Quadruped_aa block="aa"
     export function aa(): void {
         let i = 0
         // Add code here
@@ -37,8 +37,8 @@ namespace Robot {
      * TODO: describe your function here
      * @param value describe value here, eg: 5
      */
-    //% block
-    //% subcategory = BitBot_Model
+    //% subcategory=sensor
+    //% blockId=BitBot_Model block="bb"
 
     export function fib(value: number): number {
         return value <= 1 ? value : fib(value - 1) + fib(value - 2);
