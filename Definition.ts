55d178e1423756884a3f1956b9e7cae69ef77c04
Hongcheng-Shen <49156279+Hongcheng-Shen@users.noreply.github.com>
2021-06-29 17:58:45 +0800

    更新 Definition.ts

--- Definition.ts
+++ Definition.ts
@@ -1,5 +1,5 @@
 
-//----data
+//########data
 let data_tx = pins.createBuffer(38);
 let gait_mode = 0; //robot status
 let rc_spd_cmd_X = 0.00 //x_speed
@@ -14,7 +14,7 @@ let robot_mode = 0
 let robot_mode_1 = 0
 let state = 0
 
-//------SPI
+//########SPI
 let SSLen = 40
 let InfoTemp = pins.createBuffer(SSLen)
 let ToSlaveBuf = pins.createBuffer(SSLen)
@@ -23,3 +23,192 @@ let DaHeader = 0x2B
 let DaTail = 0xEE
 let usb_send_cnt = 0
 let cnt = 0
+
+//########SPI_int||SPI初始化
+function SPI_Init() {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P6, 1)
+    pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13)
+    pins.spiFrequency(1000000)
+    led.enable(false)
+}
+
+//########SPI data transmission/reception||SPI数据发送/接收
+function SPI_Send() {
+    if (state == 1) {
+        SPICom_Walk()
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P6, 0)
+        for (let i = 0; i < 200; i++);
+        for (let i = 0; i < SSLen; i++) {
+            InfoTemp[i] = pins.spiWrite(ToSlaveBuf[i])
+        }
+        pins.digitalWritePin(DigitalPin.P6, 1)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        //serial.writeBuffer(InfoTemp)
+        //serial.writeBuffer(ToSlaveBuf)
+        SPI_unpacking()
+        basic.pause(50)
+    }
+}
+//########Control data||控制数据
+function SPICom_Walk() {
+    usb_send_cnt = 0
+    let cnt_reg = 0
+    let sum = 0
+    ToSlaveBuf[usb_send_cnt++] = DaHeader; //头
+    ToSlaveBuf[usb_send_cnt++] = SSLen - 2; //固定长度
+    ToSlaveBuf[usb_send_cnt++] = 1;  //功能码
+
+    ToSlaveBuf[usb_send_cnt++] = gait_mode;
+    get_float_hex(rc_spd_cmd_X)
+    get_float_hex(rc_spd_cmd_y)
+    get_float_hex(rc_att_rate_cmd)
+    get_float_hex(rc_spd_cmd_z)
+    get_float_hex(rc_pos_cmd) //0.1
+    get_float_hex(rc_att_cmd_x)
+    get_float_hex(rc_att_cmd_y)
+    get_float_hex(rc_att_cmd)
+
+    ToSlaveBuf[SSLen - 1] = DaTail;
+}
+
+//########Data analysis||数据解析
+function SPI_unpacking() {
+    cnt = 0
+    if (InfoTemp[0] == 0x2B && InfoTemp[2] == 0x80)
+        robot_mode = InfoTemp[3]
+    //serial.writeNumber(robot_mode)
+}
+
+//########Stand in place||原地站立
+function Standing() {
+    if (robot_mode == 1)
+        return
+    gait_mode = 5
+    while (1) {
+        SPI_Send()
+        if (robot_mode == 1 || robot_mode == 0x02) {
+            gait_mode = 4
+            SPI_Send()
+            return;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//######################Data conversion||数据转换#################################################################
+function DecToBinTail(dec: number, pad: number) {
+    let bin = "";
+    let i;
+    for (i = 0; i < pad; i++) {
+        dec *= 2;
+        if (dec >= 1) {
+            dec -= 1;
+            bin += "1";
+        }
+        else {
+            bin += "0";
+        }
+    }
+    return bin;
+}
+
+function DecToBinHead(dec: number, pad: number) {
+    let bin = "";
+    let i;
+    for (i = 0; i < pad; i++) {
+        bin = parseInt((dec % 2).toString()) + bin;
+        dec /= 2;
+    }
+    return bin;
+}
+
+function get_float_hex(decString: number) {
+    let dec = decString;
+    let sign;
+    let signString;
+    let decValue = parseFloat(Math.abs(decString).toString());
+    let fraction = 0;
+    let exponent = 0;
+    let ssss = []
+
+    if (decString.toString().charAt(0) == '-') {
+        sign = 1;
+        signString = "1";
+    }
+    else {
+        sign = 0;
+        signString = "0";
+    }
+    if (decValue == 0) {
+        fraction = 0;
+        exponent = 0;
+    }
+    else {
+        exponent = 127;
+        if (decValue >= 2) {
+            while (decValue >= 2) {
+                exponent++;
+                decValue /= 2;
+            }
+        }
+        else if (decValue < 1) {
+            while (decValue < 1) {
+                exponent--;
+                decValue *= 2;
+                if (exponent == 0)
+                    break;
+            }
+        }
+        if (exponent != 0) decValue -= 1; else decValue /= 2;
+
+    }
+    let fractionString = DecToBinTail(decValue, 23);
+    let exponentString = DecToBinHead(exponent, 8);
+    let ss11 = parseInt(signString + exponentString + fractionString, 2)
+    ToSlaveBuf[usb_send_cnt++] = ((ss11 << 24) >> 24)
+    ToSlaveBuf[usb_send_cnt++] = ((ss11 << 16) >> 24)
+    ToSlaveBuf[usb_send_cnt++] = ((ss11 << 8) >> 24)
+    ToSlaveBuf[usb_send_cnt++] = ((ss11 >> 24))
+}
+
+//########################################################################################################
