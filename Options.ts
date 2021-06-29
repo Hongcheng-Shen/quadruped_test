25d5ed042c7b30f79bb6e0c80aec46f6ba4757f4
Hongcheng-Shen <49156279+Hongcheng-Shen@users.noreply.github.com>
2021-06-29 18:09:23 +0800

    更新 pxt.json, Options.ts

---
+++ Options.ts
@@ -0,0 +1,31 @@
+//############Movement direction||运动方向
+enum Mov_dir {
+    //% block="Forward"
+    For,
+    //% block="Backward"
+    Bac,
+    //% block="Turn_left"
+    Turn_l,
+    //% block="Turn_right"
+    Turn_r,
+    //% block="Shift_left"
+    Shift_l,
+    //% block="Shift_right"
+    Shift_r
+}
+
+//############Movement Angle||运动角度
+enum Mov_ang {
+    //% block="Left_swing"
+    L_swing,
+    //% block="Right_swing"
+    R_swing,
+    //% block="Look_down"
+    Look_d,
+    //% block="Look_up"
+    Look_u,
+    //% block="Yaw_left"
+    Yaw_l,
+    //% block="Yaw_right"
+    Yaw_r
+}
